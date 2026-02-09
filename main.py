f1300225ee20e0b1eb801c750b117b4d27797c92
Muhammad Al Kahfi <76249845+alkavf71@users.noreply.github.com>
2026-02-09 23:38:34 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
 
+import streamlit as st
+from modules.inspection import dashboard as inspection_dashboard
+# from modules.commissioning import dashboard as comm_dashboard # (Nanti diaktifkan)
+
+# --- PAGE CONFIG ---
+st.set_page_config(page_title="Reliability Pro Enterprise", layout="wide", page_icon="🏭")
+
+# --- SIDEBAR MENU UTAMA ---
+st.sidebar.title("🏭 Reliability Pro")
+st.sidebar.caption("Enterprise Asset Management")
+
+mode = st.sidebar.radio("Pilih Mode Kerja:", ["🛠️ INSPEKSI RUTIN", "🚀 COMMISSIONING"])
+
+st.sidebar.markdown("---")
+
+# --- ROUTING LOGIC ---
+if mode == "🛠️ INSPEKSI RUTIN":
+    # Jalankan Module Inspeksi
+    inspection_dashboard.run()
+
+elif mode == "🚀 COMMISSIONING":
+    st.title("🚀 Mode Commissioning")
+    st.info("Modul Commissioning sedang dalam pengembangan (Next Step).")
+    # comm_dashboard.run() # (Nanti diaktifkan)
